1e971313799dfb4e0d41d4fb63316856bbaf0b55
Kyle Hilbert <104146726+KyleHilbert@users.noreply.github.com>
2022-10-24 13:20:26 -0400

    wow

--- javascript.js
+++ javascript.js
@@ -12,4 +12,33 @@ function capitalizeFirstLetter(string) {
   }
 }
 
-console.log(capitalizeFirstLetter("cow")); // Foo
+function splitTest(string) {
+  const list = string.split(" ").join(".").split(".");
+  for (let i = 0; i < list.length; i++) {
+    if (list[i].length > 1) {
+      const firstPart = document.createElement("span");
+      firstPart.textContent = list[i].substring(
+        0,
+        Math.floor(list[i].length / 2)
+      );
+      firstPart.style.fontWeight = "1000";
+
+      const secondPart = document.createElement("span");
+      secondPart.textContent =
+        list[i].substring(Math.floor(list[i].length / 2), list[i].length) + " ";
+      secondPart.style.fontWeight = "500";
+
+      text.appendChild(firstPart);
+      text.appendChild(secondPart);
+    } else {
+      const firstPart = document.createElement("span");
+      firstPart.textContent = list[i] + " ";
+      firstPart.style.fontWeight = "1000";
+      text.appendChild(firstPart);
+    }
+  }
+}
+
+splitTest(
+  "Test Driven Development is the practice of writing a test for a piece of required functionality, before writing any implementation code. This test should fail when first run, and then, you write the code to get it to pass. It doesn’t have to be the most perfect code, just so long as the test passes. Once it does, you can then safely refactor your code."
+);
